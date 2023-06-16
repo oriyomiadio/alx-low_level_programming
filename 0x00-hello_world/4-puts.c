3b25693ccbedff4bb9174cac3a1b172c2701f2e5
oriyomiadio <iyandalukman2005@gmail.com>
2023-06-16 05:24:04 -0700

    hdh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,12 @@
 /**
  * Main - Entry point
  *
+ * Description: The function is to put the string
+ * and print it out to standard output
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
