2fd8be7ed06460e45436918b6453bd6d27dd44a5
oriyomiadio <iyandalukman2005@gmail.com>
2023-06-21 06:37:48 -0700

    printing putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry Point
@@ -15,6 +16,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
